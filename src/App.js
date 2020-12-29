d119d72e670f4055f9b83a09a2175fd6ed1e1af1
vivek <vivek.koyani@gmail.com>
2020-12-29 23:03:04 +0530

    add data in props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MovieList from "./MovieList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <MovieList />
     </div>
   );
 }
